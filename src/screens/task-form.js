a3777eeb0976add59496fda518e0dbe20b56678e
Alassane <alassane.fall@epitech.eu>
2019-10-28 18:33:53 +0100

    [EDIT] Form websocket v3

--- src/screens/task-form.js
+++ src/screens/task-form.js
@@ -3,18 +3,15 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import Header from '../components/header'
 import Helmet from 'react-helmet'
+import useForm from 'react-hook-form'
 import io from 'socket.io-client'
 
 function TaskForm(){
 
-    const [title, setTitle] = React.useState('')
-    const [content, setContent] = React.useState('')
-    const [author, setAuthor] = React.useState('')
-    const post = {titre: title, description: content, auteur: author}
-    const socket = io('http://localhost:4000')
-
-    function sendForm(){
-        socket.emit('createTask', post)
+    const socket = io('http://localhost:4000');
+    const { register, handleSubmit } = useForm();
+    const sendForm = values => {
+        socket.emit('createTask', values);
     }
 
     function connectSocket(){
@@ -30,20 +27,20 @@ function TaskForm(){
                     <Header/>
                 </div>
                 <h1>Nouvelle tâche</h1>
-                <form onSubmit={sendForm()}>
+                <form onSubmit={handleSubmit(sendForm)}>
                     <div className='form-group'>
                         <label>Titre</label>
-                        <input className='form-control' type='text' value={title} onChange={(e) => setTitle(e.target.value)}/>
+                        <input className='form-control' type='text' ref={register} name='cardName'/>
                         <div></div>
                     </div>
                     <div className='form-group'>
                         <label>Description</label>
-                        <input className='form-control' type='text' value={content} onChange={(e) => setContent(e.target.value)}/>
+                        <input className='form-control' type='text' ref={register} name='content'/>
                         <div></div>
                     </div>
                     <div className='form-group'>
                         <label>Auteur</label>
-                        <input className='form-control' type='text' value={author} onChange={(e) => setAuthor(e.target.value)}/>
+                        <input className='form-control' type='text' ref={register} name='author'/>
                         <div></div>
                     </div>
                     <Link to={'/task-list'} className='button_space'><button className='btn btn-danger'>Retour</button></Link>
@@ -51,6 +48,7 @@ function TaskForm(){
                 </form>
             </div>
         )
+    
 }
 
 export default connect(null,null)(TaskForm)
