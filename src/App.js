ca548924b42b3798ce5dc22028a091afd15a55c8
Reever Valerus <reever.valerus@epitech.eu>
2019-10-28 14:41:37 +0100

    register + login

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import {Provider} from 'react-redux';
 import store from './store';
-import DashBoard from './screens/DashBoard';
+import RegisterForm from './hooks/RegisterForm';
+//import { loadUser } from './store/actions/authActions';
 
 function App() {
+
+  /*useEffect(() => {
+    store.dispatch(loadUser());
+  });*/
+
   return (
     <Provider store={store}>
     <div className="App">
-      <DashBoard />
+      <RegisterForm />
     </div>
   </Provider>
   );
--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import CardMedia from '@material-ui/core/CardMedia';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import { height, textAlign } from '@material-ui/system';
-import { CardHeader, CardActionArea } from '@material-ui/core';
 
 const useStyles = makeStyles({
     header: {
